a239a2121d2264a69eea2a1c19d0dfbe0a82d2cc
H. Tony Mahadeo <HTM@dyn-10-101-010-101-172-099.prv.mc.yu.edu>
2017-10-29 09:02:45 -0400

    set up base page but doesnt render front end page yet

---
+++ MLH_hackathon/mlhAPP/mlh/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+from django.db import models
+# Leverage Django's built-in User models
+from django.contrib.auth.models import User
+
+# Create your models here.
+#class login(models.Model):
+   # email = models.CharField(max_length=28, null=False, blank=False)
+    #password = models.CharField(max_length=28, null=False, blank=False)
+
+    #def __str__(self):
+     #   """this sets the default return for this object"""
+      #  return self.description
